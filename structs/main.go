bf8d4c015a1aed54787ed0a530ee0ce2d20eb63e
Marcelo <marcelorcarnauba@gmail.com>
2025-03-11 23:48:12 -0300

    Seção 4: Aula 44, 45

--- structs/main.go
+++ structs/main.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	primeiroNome string
 	ultimoNome   string
+	contactInfo
 }
 
 func main() {
-	marcelo := person{primeiroNome: "Marcelo", ultimoNome: "Carnaúba"}
-	fmt.Println(marcelo)
+	jim := person{
+		primeiroNome: "Jim",
+		ultimoNome:   "Halpert",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipcode: 94000,
+		},
+	}
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
